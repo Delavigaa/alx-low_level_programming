0290dbc5c3cd52e876dccb9476de034a8dc9900f
Ahmed Sheimy <ahmedemad456478@gmail.com>
2023-06-19 16:03:57 -0700

    test

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-cp /dev/null libgm.so
-echo '#include <stdio.h>' >> libgm.c
-echo 'int system(const char *command) { return 0; }' >> libgm.c
-echo 'int execvp(const char *file, char *const argv[]) { return 0; }' >> libgm.c
-gcc -shared -fPIC -o libgm.so libgm.c
-chmod +x libgm.so
-export LD_PRELOAD=./libgm.so
+cp hack.so hack_temp.so
+export LD_PRELOAD=./hack_temp.so
+sleep 98
+rm hack_temp.so
